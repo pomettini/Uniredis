c0dcc0615c8b8cb762e0de705b5f2ff131dfb0dc
Giorgio Pomettini <giorgio.freespire@gmail.com>
2018-06-18 16:53:00 +0200

    First commit

---
+++ RedisClient/Assets/Example.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Example : MonoBehaviour 
+{
+	void Start () 
+	{
+		using (Redis redis = new Redis("192.168.23.127"))
+		{
+			Debug.Log(redis.SetKey("pomettini", "ciao"));
+			Debug.Log(redis.GetKey("pomettini"));
+		}
+	}
+}
---
+++ RedisClient/Assets/Redis.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System;
+using System.IO;
+using System.Text;
+using UnityEngine;
+
+public class Redis : IDisposable
+{
+	[HideInInspector]
+	public string Host;
+	[HideInInspector]
+	public int Port;
+
+	private Socket socket;
+	private byte[] data;
+
+	private const string CRLF = "\r\n";
+
+	public Redis (string host)
+	{
+		this.Host = host;
+		this.Port = 6379;
+	}
+
+	public class RedisException : Exception
+	{
+		public RedisException(string message) : base(message) {}
+	}
+
+	private void Connect ()
+	{
+		socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+		socket.Connect(Host, Port);
+		// socket.Blocking = false;
+
+		if (!socket.Connected)
+		{
+			socket.Close();
+			socket = null;
+			return;
+		}
+
+		data = new byte[4096];
+	}
+
+	public void Disconnect ()
+	{
+		socket.Close();
+	}
+
+	public string GetKey (string key)
+	{
+		String query = String.Format("GET {0}", key);
+		return Query(query);
+	}
+
+	public bool SetKey (string key, string value)
+	{
+		String query = String.Format("SET {0} \"{1}\"", key, value);
+		string result = Query(query);
+
+		if (result != "+OK" + CRLF)
+		{
+			throw new RedisException(result);
+		}
+
+		return true;
+	}
+
+	private string Query (string query)
+	{
+		if (socket == null)
+			Connect();
+
+		string result = String.Empty;
+
+		while (true)
+		{
+			data = Encoding.ASCII.GetBytes(query + CRLF);
+			int length = socket.Send(data);
+
+			byte[] dataReceived = new byte[4096];
+			int dataReceiveLength = socket.Receive(dataReceived);
+
+			if (length <= 0)
+			{
+				break;
+			}
+
+			result = Encoding.ASCII.GetString(dataReceived, 0, dataReceiveLength);
+			return result;
+		}
+
+		return result;
+	}
+
+	public void Dispose ()
+	{
+		Dispose(true);
+		GC.SuppressFinalize(this);
+	}
+
+	protected virtual void Dispose (bool disposing)
+	{
+		if (disposing)
+		{
+			Query("QUIT");
+			socket.Close();
+			socket = null;
+		}
+	}
+}
